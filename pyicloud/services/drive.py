261fc3902c5eaa8ae14368a02cd4d8b1593b6c28
gordon.aspin@gmail.com <gordon.aspin@gmail.com>
2025-02-24 13:02:23 -0500

    implement get_app_node

--- pyicloud/services/drive.py
+++ pyicloud/services/drive.py
@@ -25,6 +25,10 @@ class DriveService:
         self.params = dict(params)
         self._root = None
 
+    def get_app_node(self, app_id, folder="documents"):
+        """Returns the node of the app (ubiquity)"""
+        return DriveNode(self, self.get_node_data("FOLDER::" + app_id + "::" + folder))
+
     def _get_token_from_cookie(self):
         for cookie in self.session.cookies:
             if cookie.name == "X-APPLE-WEBAUTH-VALIDATE":
@@ -34,7 +38,7 @@ class DriveService:
                 return {"token": match.group(1)}
         raise Exception("Token cookie not found")
 
-    def get_node_data(self, node_id):
+    def get_node_data(self, drivewsid):
         """Returns the node data."""
         request = self.session.post(
             self._service_root + "/retrieveItemDetailsInFolders",
@@ -42,7 +46,7 @@ class DriveService:
             data=json.dumps(
                 [
                     {
-                        "drivewsid": "FOLDER::com.apple.CloudDocs::%s" % node_id,
+                        "drivewsid": drivewsid,
                         "partialData": False,
                     }
                 ]
@@ -51,12 +55,12 @@ class DriveService:
         self._raise_if_error(request)
         return request.json()[0]
 
-    def get_file(self, file_id, **kwargs):
+    def get_file(self, file_id, zone="com.apple.CloudDocs", **kwargs):
         """Returns iCloud Drive file."""
         file_params = dict(self.params)
         file_params.update({"document_id": file_id})
         response = self.session.get(
-            self._document_root + "/ws/com.apple.CloudDocs/download/by_id",
+            self._document_root + f"/ws/{zone}/download/by_id",
             params=file_params,
         )
         self._raise_if_error(response)
@@ -77,7 +81,7 @@ class DriveService:
         self._raise_if_error(request)
         return request.json()["items"]
 
-    def _get_upload_contentws_url(self, file_object, **kwargs):
+    def _get_upload_contentws_url(self, file_object, zone="com.apple.CloudDocs", **kwargs):
         """Get the contentWS endpoint URL to add a new file."""
         content_type = mimetypes.guess_type(file_object.name)[0]
         if content_type is None:
@@ -93,7 +97,7 @@ class DriveService:
         file_params.update(self._get_token_from_cookie())
 
         request = self.session.post(
-            self._document_root + "/ws/com.apple.CloudDocs/upload/web",
+            self._document_root + f"/ws/{zone}/upload/web",
             params=file_params,
             headers={"Content-Type": "text/plain"},
             data=json.dumps(
@@ -108,7 +112,7 @@ class DriveService:
         self._raise_if_error(request)
         return (request.json()[0]["document_id"], request.json()[0]["url"])
 
-    def _update_contentws(self, folder_id, sf_info, document_id, file_object, **kwargs):
+    def _update_contentws(self, folder_id, sf_info, document_id, file_object, zone="com.apple.CloudDocs", **kwargs):
         data = {
             "data": {
                 "signature": sf_info["fileChecksum"],
@@ -138,7 +142,7 @@ class DriveService:
             data["data"].update({"receipt": sf_info["receipt"]})
 
         request = self.session.post(
-            self._document_root + "/ws/com.apple.CloudDocs/update/documents",
+            self._document_root + f"/ws/{zone}/update/documents",
             params=self.params,
             headers={"Content-Type": "text/plain"},
             data=json.dumps(data),
@@ -146,14 +150,14 @@ class DriveService:
         self._raise_if_error(request)
         return request.json()
 
-    def send_file(self, folder_id, file_object, **kwargs):
+    def send_file(self, folder_id, file_object, zone="com.apple.CloudDocs", **kwargs):
         """Send new file to iCloud Drive."""
-        document_id, content_url = self._get_upload_contentws_url(file_object, **kwargs)
+        document_id, content_url = self._get_upload_contentws_url(file_object, zone, **kwargs)
 
         request = self.session.post(content_url, files={file_object.name: file_object})
         self._raise_if_error(request)
         content_response = request.json()["singleFile"]
-        self._update_contentws(folder_id, content_response, document_id, file_object, **kwargs)
+        self._update_contentws(folder_id, content_response, document_id, file_object, zone, **kwargs)
 
     def create_folders(self, parent, name):
         """Creates a new iCloud Drive folder"""
@@ -240,7 +244,7 @@ class DriveService:
     def root(self):
         """Returns the root node."""
         if not self._root:
-            self._root = DriveNode(self, self.get_node_data("root"))
+            self._root = DriveNode(self, self.get_node_data("FOLDER::com.apple.CloudDocs::root"))
         return self._root
 
     def __getattr__(self, attr):
@@ -283,7 +287,7 @@ class DriveNode:
         """Gets the node children."""
         if not self._children or force:
             if "items" not in self.data or force:
-                self.data.update(self.connection.get_node_data(self.data["docwsid"]))
+                self.data.update(self.connection.get_node_data(self.data["drivewsid"]))
             if "items" not in self.data:
                 raise KeyError("No items in folder, status: %s" % self.data["status"])
             self._children = [
@@ -330,11 +334,11 @@ class DriveNode:
             response = Response()
             response.raw = io.BytesIO()
             return response
-        return self.connection.get_file(self.data["docwsid"], **kwargs)
+        return self.connection.get_file(self.data["docwsid"], zone=self.data["zone"], **kwargs)
 
     def upload(self, file_object, **kwargs):
         """Upload a new file."""
-        return self.connection.send_file(self.data["docwsid"], file_object, **kwargs)
+        return self.connection.send_file(self.data["docwsid"], file_object, zone=self.data["zone"], **kwargs)
 
     def dir(self):
         """Gets the node list of directories."""
