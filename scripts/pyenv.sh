bd9c1e91389c83e2fb35253514ae22b3a57cd46d
Tim Laing <11019084+timlaing@users.noreply.github.com>
2025-06-14 20:50:12 +0000

    fix: correct shebang in pyenv script and update sonar project properties for Python versions

--- scripts/pyenv.sh
+++ scripts/pyenv.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/zsh
+#!/usr/bin/env zsh
 # Install pyenv
 curl -fsSL https://pyenv.run | bash
 export PATH="$HOME/.pyenv/bin:$PATH"
 export PYENV_ROOT="$HOME/.pyenv"
-~/.pyenv/bin/pyenv init >> ~/.zshrc 2>&1
-eval "$(pyenv init - zsh)"
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+eval "$(pyenv init -)"
 
 pyenv install 3.9 3.10 3.11 3.12 3.13
