ef9e24e0fdc3904f8075cde87fdf7ca1d726267a
Tim Laing <11019084+timlaing@users.noreply.github.com>
2026-01-11 18:27:07 +0000

    Fix quoting in virtual environment activation in setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,8 +3,8 @@ set -e
 
 export UV_LINK_MODE=copy
 uv venv --seed --clear && uv pip install -r requirements_all.txt
-grep -qx "source ${WORKSPACE_DIRECTORY}/.venv/bin/activate" ~/.zshrc || cat <<EOF >>~/.zshrc
+grep -qx "source \"${WORKSPACE_DIRECTORY}/.venv/bin/activate\"" ~/.zshrc || cat <<EOF >>~/.zshrc
 if [ -f "${WORKSPACE_DIRECTORY}/.venv/bin/activate" ]; then
-  source ${WORKSPACE_DIRECTORY}/.venv/bin/activate
+  source "${WORKSPACE_DIRECTORY}/.venv/bin/activate"
 fi
 EOF
