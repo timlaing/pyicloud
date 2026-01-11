7a1ed057859c60532e5260564b5af8b4e4d75a86
Tim Laing <11019084+timlaing@users.noreply.github.com>
2026-01-11 18:23:43 +0000

    Fix grep command in setup script to use fixed string matching for virtual environment activation

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@ set -e
 
 export UV_LINK_MODE=copy
 uv venv --seed --clear && uv pip install -r requirements_all.txt
-grep -qxF 'source ${WORKSPACE_DIRECTORY}/.venv/bin/activate' ~/.zshrc || cat <<EOF >>~/.zshrc
+grep -qx "source ${WORKSPACE_DIRECTORY}/.venv/bin/activate" ~/.zshrc || cat <<EOF >>~/.zshrc
 if [ -f "${WORKSPACE_DIRECTORY}/.venv/bin/activate" ]; then
   source ${WORKSPACE_DIRECTORY}/.venv/bin/activate
 fi
