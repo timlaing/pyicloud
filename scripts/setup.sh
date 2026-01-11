d20aba7cc01cd795813801e53456011d8d0f86ae
Tim Laing <11019084+timlaing@users.noreply.github.com>
2026-01-11 18:03:59 +0000

    Enhance dev container configuration and update pre-commit hooks versions

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,8 +3,8 @@ set -e
 
 export UV_LINK_MODE=copy
 uv venv --seed --clear && uv pip install -r requirements_all.txt
-grep -qxF 'source /workspaces/pyicloud/.venv/bin/activate' ~/.zshrc || cat <<EOF >>~/.zshrc
-if [ -f "/workspaces/pyicloud/.venv/bin/activate" ]; then
-  source /workspaces/pyicloud/.venv/bin/activate
+grep -qxF 'source ${WORKSPACE_DIRECTORY}/.venv/bin/activate' ~/.zshrc || cat <<EOF >>~/.zshrc
+if [ -f "${WORKSPACE_DIRECTORY}/.venv/bin/activate" ]; then
+  source ${WORKSPACE_DIRECTORY}/.venv/bin/activate
 fi
 EOF
